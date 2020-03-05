12c5f231d2d02a355e141e4450998f85a5d28e4f
tnoff <1326564+tnoff@users.noreply.github.com>
2020-03-04 22:43:59 -0800

    Move bot function into main function

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -8,25 +8,26 @@ from discord.ext import commands
 from discord_bot import functions
 from discord_bot.utils import get_logger
 
-bot = commands.Bot(command_prefix='!')
-
-logger = get_logger(__name__, '/var/log/discord-bot/discord.log')
-
-@bot.command()
-async def hello(ctx):
-    _, message = functions.hello(ctx, logger)
-    await ctx.send(message)
-
-@bot.command()
-async def roll(ctx, number):
-    _status, message = fucntions.roll(ctx, logger, number)
-    await ctx.send(message)
-    
-@bot.command()
-async def windows(ctx):
-    _, message = functions.windows(ctx, logger)
-    await ctx.send(message)
-
 def main():
+    discord_token = os.environ['DISCORD_TOKEN']
+    bot = commands.Bot(command_prefix='!')
+
+    logger = get_logger(__name__, '/var/log/discord-bot/discord.log')
+
+    @bot.command()
+    async def hello(ctx):
+        _, message = functions.hello(ctx, logger)
+        await ctx.send(message)
+
+    @bot.command()
+    async def roll(ctx, number):
+        _status, message = fucntions.roll(ctx, logger, number)
+        await ctx.send(message)
+
+    @bot.command()
+    async def windows(ctx):
+        _, message = functions.windows(ctx, logger)
+        await ctx.send(message)
+
     discord_token = os.environ['DISCORD_TOKEN']
     bot.run(discord_token)
