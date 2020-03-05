1701125792398e1bdda9c2b4ef0684f8afe106b4
tnoff <1326564+tnoff@users.noreply.github.com>
2020-03-04 22:52:26 -0800

    Add cli args

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import re
 import random
@@ -6,13 +7,30 @@ import discord
 from discord.ext import commands
 
 from discord_bot import functions
+from discord_bot.exceptions import DiscordBotException
 from discord_bot.utils import get_logger
 
+LOG_FILE_DEFAULT = '/var/log/discord-bot/discord.log'
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Discord Bot Runner")
+    parser.add_argument("--log-file", "-l", default=LOG_FILE_DEFAULT,
+                        help="Logging file")
+    parser.add_argument("--discord-token", "-t", help="Discord token, defaults to DISCORD_TOKEN env arg")
+    return parser.parse_args()
+
 def main():
-    discord_token = os.environ['DISCORD_TOKEN']
+    args = parse_args()
+    # Check for token
+    if args.discord_token is None:
+        try:
+            args.discord_token = os.environ['DISCORD_TOKEN']
+        except KeyError:
+            raise DiscordBotException("No discord token given")
+
+    logger = get_logger(__name__, args.log_file)
     bot = commands.Bot(command_prefix='!')
 
-    logger = get_logger(__name__, '/var/log/discord-bot/discord.log')
 
     @bot.command()
     async def hello(ctx):
@@ -29,5 +47,4 @@ def main():
         _, message = functions.windows(ctx, logger)
         await ctx.send(message)
 
-    discord_token = os.environ['DISCORD_TOKEN']
-    bot.run(discord_token)
+    bot.run(args.discord_token)
