57fb46dad90930283534cc94ecbae4d0e7f01555
tnoff <1326564+tnoff@users.noreply.github.com>
2020-03-04 23:03:48 -0800

    Add config file support

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -1,4 +1,5 @@
 import argparse
+from configparser import NoSectionError, NoOptionError, SafeConfigParser
 import os
 import re
 import random
@@ -11,27 +12,54 @@ from discord_bot.exceptions import DiscordBotException
 from discord_bot.utils import get_logger
 
 LOG_FILE_DEFAULT = '/var/log/discord-bot/discord.log'
+HOME_PATH = os.path.expanduser("~")
+CONFIG_PATH_DEFAULT = os.path.join(HOME_PATH, ".discord-bot.conf")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Discord Bot Runner")
+    parser.add_argument("--config-file", "-c", default=CONFIG_PATH_DEFAULT, help="Config file")
     parser.add_argument("--log-file", "-l", default=LOG_FILE_DEFAULT,
                         help="Logging file")
     parser.add_argument("--discord-token", "-t", help="Discord token, defaults to DISCORD_TOKEN env arg")
     return parser.parse_args()
 
-def main():
-    args = parse_args()
-    # Check for token
-    if args.discord_token is None:
+def read_config(config_file):
+    if config_file is None:
+        return dict()
+    parser = SafeConfigParser()
+    parser.read(config_file)
+    mapping = {
+        'log_file' : ['general', 'log_file'],
+        'discord_token' : ['general', 'discord_token'],
+    }
+    return_data = dict()
+    for key_name, args in mapping.items():
         try:
-            args.discord_token = os.environ['DISCORD_TOKEN']
-        except KeyError:
-            raise DiscordBotException("No discord token given")
+            value = parser.get(*args)
+        except (NoSectionError, NoOptionError):
+            value = None
+        return_data[key_name] = value
+    return return_data
 
-    logger = get_logger(__name__, args.log_file)
+def main():
+    # First get cli args
+    args = parse_args()
+    # Load settings
+    settings = read_config(args.config_file)
+    # Override settings if cli args passed
+    for key, item in vars(args):
+        settings[key] = item
+
+    # Check for token
+    if settings['discord_token'] is None:
+        raise DiscordBotException("No discord token given")
+
+    # Setup vars
+    logger = get_logger(__name__, settings['log_file'])
     bot = commands.Bot(command_prefix='!')
 
-
+    # Bot commands
     @bot.command()
     async def hello(ctx):
         _, message = functions.hello(ctx, logger)
@@ -47,4 +75,5 @@ def main():
         _, message = functions.windows(ctx, logger)
         await ctx.send(message)
 
+    # Run bot
     bot.run(args.discord_token)
--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setuptools.setup(
         ]
     },
     packages=setuptools.find_packages(exclude=['tests']),
-    version='0.0.5',
+    version='0.0.6',
 )
