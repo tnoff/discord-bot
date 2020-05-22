bc8188cfcee9acfc83e8ad53221677aafdb8fc8b
tnoff <1326564+tnoff@users.noreply.github.com>
2020-05-22 05:52:15 +0000

    Fix weird nullable issue

--- discord_bot/discord_bot/database.py
+++ discord_bot/discord_bot/database.py
@@ -7,18 +7,18 @@ class Server(BASE):
     __tablename__ = 'discord_server'
 
     id = Column(BigInteger, primary_key=True)
-    name = Column(String(1024))
+    name = Column(String(1024), nullable=False)
 
 class User(BASE):
     __tablename__ = 'discord_user'
 
     id = Column(BigInteger, primary_key=True)
-    name = Column(String(1024))
+    name = Column(String(1024), nullable=False)
 
 class TwitterSubscription(BASE):
     __tablename__ = 'twitter_subscription'
 
     id = Column(Integer, primary_key=True)
-    twitter_user_id = Column(String(1024))
-    webhook_url = Column(String(2048))
+    twitter_user_id = Column(String(1024), nullable=False)
+    webhook_url = Column(String(2048), nullable=False)
     last_post = Column(BigInteger)
