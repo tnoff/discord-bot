aabd9d92cd904898cc4b7ad3b7e6efbb4c67dc54
tnoff <1326564+tnoff@users.noreply.github.com>
2021-01-06 05:02:32 +0000

    Add markov bot cog
    - Turn on/off markov functionality for text channel
    - Read messages, and create leader/follower message pairs
    - Keep count of how often leader/follower happens

---
+++ discord_bot/discord_bot/cogs/markov.py
@@ -0,0 +1,177 @@
+import asyncio
+import re
+
+from discord import TextChannel
+from discord.ext import commands
+
+from discord_bot.cogs.common import CogHelper
+from discord_bot.database import MarkovChannel
+from discord_bot.database import MarkovRelation, MarkovWord
+
+# https://srome.github.io/Making-A-Markov-Chain-Twitter-Bot-In-Python/
+def build_transition_matrix(corpus):
+    '''
+    corpus  :   Input message
+
+    Returns dictionary of each word, and words following that word
+    Ex:
+    {
+        "hello": ["there", "my"],
+        "my": ["friend."],
+        "there": ["my"],
+        "friend.": ["hello"],
+    }
+    '''
+    corpus = corpus.split(' ')
+    transitions = {}
+    for (k, word) in enumerate(corpus):
+        if k != len(corpus) - 1: # Deal with last word
+            next_word = corpus[k+1]
+        else:
+            next_word = corpus[0] # To loop back to the beginning
+
+        if word not in transitions:
+            transitions[word] = []
+
+        transitions[word].append(next_word)
+    return transitions
+
+class Markov(CogHelper):
+    '''
+    Save markov relations to a database periodically
+    '''
+    def __init__(self, bot, db_session, logger):
+        super().__init__(bot, db_session, logger)
+        self.bot.loop.create_task(self.wait_loop())
+
+    def __ensure_word(self, word, channel):
+        markov_word = self.db_session.query(MarkovWord).\
+                filter(MarkovWord.word == word).\
+                filter(MarkovWord.channel_id == channel.id).first()
+        if markov_word:
+            return markov_word
+        if len(word) > 1024:
+            self.logger.warning(f'Cannot add word "{word}", is too long')
+            return None
+        new_word = MarkovWord(word=word, channel_id=channel.id)
+        self.db_session.add(new_word)
+        self.db_session.commit()
+        self.db_session.flush()
+        return new_word
+
+    def __ensure_relation(self, leader, follower):
+        markov_relation = self.db_session.query(MarkovRelation).\
+                filter(MarkovRelation.leader_id == leader.id).\
+                filter(MarkovRelation.follower_id == follower.id).first()
+        if markov_relation:
+            return markov_relation
+        new_relation = MarkovRelation(leader_id=leader.id,
+                                      follower_id=follower.id,
+                                      count=0)
+        self.db_session.add(new_relation)
+        self.db_session.commit()
+        self.db_session.flush()
+        return new_relation
+
+    def __build_and_save_relations(self, message, channel):
+        transitions = build_transition_matrix(message)
+        for leader, followers in transitions.items():
+            leader_word = self.__ensure_word(leader, channel)
+            if leader_word is None:
+                continue
+            for follower in followers:
+                follower_word = self.__ensure_word(follower, channel)
+                if follower_word is None:
+                    continue
+                relation = self.__ensure_relation(leader_word, follower_word)
+                relation.count += 1
+                self.db_session.commit()
+
+    async def wait_loop(self):
+        '''
+        Our main loop.
+        '''
+        await self.bot.wait_until_ready()
+
+        while not self.bot.is_closed():
+            for markov_channel in self.db_session.query(MarkovChannel).all():
+                channel = await self.bot.fetch_channel(markov_channel.channel_id)
+                if not markov_channel.last_message_id:
+                    messages = await channel.history(limit=100).flatten()
+                else:
+                    last_message = await channel.fetch_message(markov_channel.last_message_id)
+                    messages = await channel.history(after=last_message).flatten()
+
+                for message in messages:
+                    # Skip messages that @ someone
+                    if message.mentions or message.role_mentions or message.channel_mentions:
+                        continue
+                    if message.author.bot:
+                        continue
+                    # If message begins with '!', assume it was a bot command
+                    if message.content[0] == '!':
+                        continue
+                    # Remove links from message text
+                    message_text = re.sub(r'^https?:\/\/.*[\r\n]*', '',
+                                          message.content, flags=re.MULTILINE)
+                    if not message_text:
+                        continue
+                    # Use lower() so we can re-use words better
+                    message_text = message_text.lower()
+                    self.__build_and_save_relations(message_text, channel)
+
+                    markov_channel.last_message_id = message.id
+                    self.db_session.commit()
+
+            await asyncio.sleep(300)
+
+    @commands.group(name='markov', invoke_without_command=False)
+    async def markov(self, ctx):
+        '''
+        Markov functions
+        '''
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid sub command passed...')
+
+    @markov.command(name='on')
+    async def on(self, ctx):
+        '''
+        Turn markov on for channel
+        '''
+        # Ensure channel not already on
+        markov = self.db_session.query(MarkovChannel).\
+            filter(MarkovChannel.channel_id == ctx.channel.id).\
+            filter(MarkovChannel.server_id == ctx.guild.id).first()
+
+        if markov:
+            return await ctx.send('Channel already has markov turned on')
+
+        channel = await self.bot.fetch_channel(ctx.channel.id)
+        if not isinstance(channel, TextChannel):
+            await ctx.send('Channel is not text channel, cannot turn on markov')
+
+        new_markov = MarkovChannel(channel_id=ctx.channel.id,
+                                   server_id=ctx.guild.id,
+                                   last_message_id=None)
+        self.db_session.add(new_markov)
+        self.db_session.commit()
+
+        return await ctx.send('Markov turned on for channel')
+
+    @markov.command(name='off')
+    async def off(self, ctx):
+        '''
+        Turn markov off for channel
+        '''
+        # Ensure channel not already on
+        markov = self.db_session.query(MarkovChannel).\
+            filter(MarkovChannel.channel_id == ctx.channel.id).\
+            filter(MarkovChannel.server_id == ctx.guild.id).first()
+
+        if not markov:
+            return await ctx.send('Channel does not have markov turned on')
+
+        self.db_session.delete(markov)
+        self.db_session.commit()
+
+        return await ctx.send('Markov turned off for channel')
