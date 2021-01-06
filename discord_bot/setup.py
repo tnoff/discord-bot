1875b73228504af51030dd4d4d620bc7b5063675
tnoff <1326564+tnoff@users.noreply.github.com>
2021-01-06 04:07:23 +0000

    Remove unecessary packages

--- discord_bot/setup.py
+++ discord_bot/setup.py
@@ -8,12 +8,10 @@ setuptools.setup(
     install_requires=[
         'cryptography >= 2.9.2',
         'discord >= 1.0.1',
-        'discord.py >= 1.3.4',
         'moviepy >= 1.0.3',
-        'mysqlclient >= 1.4.6',
         'numpy >= 1.18.5',
         'PyMySQL >= 0.9.3',
-        'PyNaCl >= 1.3.0',
+        'PyNaCl >= 1.4.0',
         'python-twitter >= 3.5',
         'SQLAlchemy >= 1.3.13',
         'youtube-dl >= 2020.11.21.1',
@@ -24,5 +22,5 @@ setuptools.setup(
         ]
     },
     packages=setuptools.find_packages(exclude=['tests']),
-    version='1.4.5',
+    version='1.4.6',
 )
