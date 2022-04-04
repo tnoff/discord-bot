9730fee444c84a2f3e98c1a27e59528f3f11f1c3
tnoff <1326564+tnoff@users.noreply.github.com>
2022-04-03 21:55:10 -0700

    Allow plugins from requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import setuptools
+import os
+
+THIS_DIR = os.path.dirname(__file__)
+REQUIREMENTS_FILES = [os.path.join(THIS_DIR, 'requirements.txt')]
+
+for root, dirs, files in os.walk(os.path.join(THIS_DIR, 'cogs/plugins')):
+    for name in files:
+        if 'requirements.txt' in name.lower():
+            REQUIREMENTS_FILES.append(os.path.join(root, name))
+
+required = []
+for file_name in REQUIREMENTS_FILES:
+    with open(file_name) as f:
+        required += f.read().splitlines()
 
 setuptools.setup(
     name='discord_bot',
     description='Discord Bot',
     author='Tyler D. North',
     author_email='ty_north@yahoo.com',
-    install_requires=[
-        # TODO move this to requires.txt
-        # Then allow for additional files for plugins?
-        'cryptography >= 2.9.2',
-        'discord >= 1.0.1',
-        'PyMySQL >= 1.0.2',
-        'PyNaCl >= 1.4.0',
-        'python-twitter >= 3.5',
-        'pathlib >= 1.0.1',
-        'SQLAlchemy >= 1.4.18',
-        'yt-dlp >= 2021.10.10',
-    ],
+    install_requires=required,
     entry_points={
         'console_scripts' : [
             'discord-bot = discord_bot.run_bot:main',
