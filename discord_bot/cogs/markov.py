2e9136658f191b5737808b3b24166b765c92bd4a
tnoff <1326564+tnoff@users.noreply.github.com>
2024-06-23 09:15:23 -0700

    Add more retries on emoji check

--- discord_bot/cogs/markov.py
+++ discord_bot/cogs/markov.py
@@ -198,7 +198,8 @@ class Markov(CogHelper):
             self.logger.debug(f'Markov :: Checking channel id: {markov_channel.channel_id}, server id: {markov_channel.server_id}')
             channel = await async_retry_discord_message_command(self.bot.fetch_channel, markov_channel.channel_id)
             server = await async_retry_discord_message_command(self.bot.fetch_guild, markov_channel.server_id)
-            emojis = await async_retry_discord_message_command(server.fetch_emojis)
+            # Not sure why but this check in particular seems especially flakey
+            emojis = await async_retry_discord_message_command(server.fetch_emojis, max_retries: 5)
             self.logger.info('Markov :: Gathering markov messages for '
                              f'channel {markov_channel.channel_id}')
             # Start at the beginning of channel history,
