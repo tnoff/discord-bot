97da20a4cc745651c359b9536e904b0dcaec3ad1
Tyler North <me@tyler-north.com>
2025-10-10 23:35:08 -0700

    fix: bump max message length down to 1800

--- discord_bot/common.py
+++ discord_bot/common.py
@@ -1,3 +1,3 @@
 # https://support.discord.com/hc/en-us/articles/360034632292-Sending-Messages
-# Is 2000, set to 1975 to be safe
-DISCORD_MAX_MESSAGE_LENGTH = 1975
+# Is 2000, set to 1800 to be safe
+DISCORD_MAX_MESSAGE_LENGTH = 1800
