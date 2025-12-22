4f79ee521b946fbb77e538cb5a2ff953cc47da23
Tyler North <me@tyler-north.com>
2025-12-21 16:57:34 -0800

    Text check (#570)
    * fix: update message length to be safe
    
    * fix: update workflow call

--- discord_bot/common.py
+++ discord_bot/common.py
@@ -1,3 +1,3 @@
 # https://support.discord.com/hc/en-us/articles/360034632292-Sending-Messages
-# Is 2000, set to 1950 to be safe
-DISCORD_MAX_MESSAGE_LENGTH = 1950
+# Is 2000, set to 1900 to be safe
+DISCORD_MAX_MESSAGE_LENGTH = 1900
